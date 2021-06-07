7287e04c99c23db32c232d658af0b7659fd4f405
JRB <jrb@abv.bg>
2021-06-07 23:02:17 +0300

    L2- Lab fill-Add

---
+++ L2_Basic_CRUD/L2_Lab.sql
@@ -0,0 +1,14 @@
+# 01-Select Employee Information
+SELECT `id`, `first_name`, `last_name`, `job_title` FROM `employees`
+ORDER BY `id`;
+
+# 02-Select Employees with Filter
+SELECT `id`, concat_ws(' ', `first_name`, `last_name`) AS 'full_name' , `job_title`, `salary`
+FROM `employees`
+WHERE `salary` > '1000,00'
+ ORDER BY `id`;
+ 
+ # 03-Update Employees Salary
+ SELECT * FROM `employees`
+ WHERE `job_title` = 'Manager'
+-- UPDATE `employees` SET `salary` = `salary` + 100; 
